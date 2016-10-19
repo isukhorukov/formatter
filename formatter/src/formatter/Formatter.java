cd2e1fbf363977e8e46421d7370920bfd4d80b63
isukhorukov <sukhorukov.ilia@gmail.com>
2016-10-19 14:22:37 +0600

    open bracket detector works

--- formatter/src/formatter/Formatter.java
+++ formatter/src/formatter/Formatter.java
@@ -4,23 +4,37 @@ import java.io.*;
 
 public class Formatter {
     
-    static final int SPACE    = 32; // space
-    static final int TRANSFER = 10; // enter
+    static final int SPACE         = 32;   // space
+    static final int TRANSFER      = 10;   // enter
+    static final int OPEN_BRACKET  = 123;  // {
+    static final int CLOSE_BRACKET = 125;  // }
+    static final int SEMICOLON     = 59;   // ;
     
     public static void main(String[] args) {
-        String from = "public Class A { static int a = 5; static int b = 2; }";
-        int ch;
         
-       
-        String charCode = "{";
-        System.out.println((int)charCode.charAt(0));
+        String s = "public Class A { static int a = 5; static int b = 2; }";
+        StringBuilder from = new StringBuilder(s);
+        int i = 0;
         
-   //     while ((int)space.charAt(i) == SPACE || 
-   //              (int)space.charAt(i) == TRANSFER) {
-    
-  //              }
+//        int ch;    
+//        String charCode = "}";
+//        System.out.println((int)charCode.charAt(0));
+      
+        while (i < from.length()) {
+            if ((int)from.charAt(i) == OPEN_BRACKET) {
+                i++;
+                while ((int)from.charAt(i) == SPACE || 
+                       (int)from.charAt(i) == TRANSFER) {
+                    from.deleteCharAt(i);
+                }
+                from.insert(i, (char)TRANSFER);
+                i++;
+                from.insert(i, (char)SPACE).insert(i, (char)SPACE).insert(i, (char)SPACE).insert(i, (char)SPACE);
+            }
+            i++;
+        }        
         
+        System.out.println(from);
         
     }
-    
 }
