2f7152a62d20e89b6d563d46e6773c78aa8e0755
isukhorukov <sukhorukov.ilia@gmail.com>
2016-10-19 13:29:05 +0600

    char code

--- formatter/src/formatter/Formatter.java
+++ formatter/src/formatter/Formatter.java
@@ -1,9 +1,26 @@
 package formatter;
 
-public class Formatter {
+import java.io.*;
 
+public class Formatter {
+    
+    static final int SPACE    = 32; // space
+    static final int TRANSFER = 10; // enter
+    
     public static void main(String[] args) {
-        System.out.println("Initial code.");
+        String from = "public Class A { static int a = 5; static int b = 2; }";
+        int ch;
+        
+       
+        String charCode = "\n";
+        System.out.println((int)charCode.charAt(0));
+        
+   //     while ((int)space.charAt(i) == SPACE || 
+   //              (int)space.charAt(i) == TRANSFER) {
+    
+  //              }
+        
+        
     }
     
 }
